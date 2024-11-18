ab22a89d340d1d8bbedf2827c16a769f3482c0e3
Alan934 <sanjurjoalan77@gmail.com>
2024-11-18 17:04:49 -0300

    client

--- src/flow/flow.controller.ts
+++ src/flow/flow.controller.ts
@@ -1,10 +1,10 @@
-// import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-// import { FlowService } from './flow.service';
-// import { CreateFlowDto } from './dto/create-flow.dto';
-// import { UpdateFlowDto } from './dto/update-flow.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { FlowService } from './flow.service';
+import { CreateFlowDto } from './dto/create-flow.dto';
+import { UpdateFlowDto } from './dto/update-flow.dto';
 
-// @Controller('flow')
-// export class FlowController {
-//   constructor(private readonly flowService: FlowService) {}
+@Controller('flow')
+export class FlowController {
+  constructor(private readonly flowService: FlowService) {}
 
-// }
+}
--- src/pricing-plan/dto/update-pricing-plan.dto.ts
+++ src/pricing-plan/dto/update-pricing-plan.dto.ts
@@ -3,4 +3,8 @@ import { CreatePricingPlanDto } from './create-pricing-plan.dto';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdatePricingPlanDto extends PartialType(CreatePricingPlanDto) {
+    
+    @IsString()
+    @IsNotEmpty()
+    id: string;
 }
