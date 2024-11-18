d1c4c03f2c566170c0375478714c0711b583699e
NicolasVelasco <nicolas.velasco@alumnos.frm.utn.edu.ar>
2024-11-18 13:14:46 -0300

    Authenticated

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,32 +1,45 @@
-import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Req, Request, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signUp.dto';
-import { AuthGuard } from './guards/auth.guard';
-import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private jwtService: JwtService,
+  ) {}
 
   @Post('signIn')
   @ApiBody({
     type: SignInDto,
-  examples: {
-    example: {
-      value: {
-        email: 'test@gmail.com',
-        pass: 'password',
-      }
-    }
-  }})
+    examples: {
+      example: {
+        value: {
+          email: 'test@gmail.com',
+          pass: 'password',
+        },
+      },
+    },
+  })
   @ApiTags('Profile')
   async signIn(@Body() signInDto: SignInDto) {
     return await this.authService.signIn(signInDto.email, signInDto.pass);
   }
 
-  @UseGuards(AuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.profile;
+  @ApiTags('Authenticated')
+  @Post('authenticated')
+  async getProfile(@Req() req, @Res() res) {
+    try {
+      const [type, token] = req.headers.authorization?.split(' ') ?? [];
+      const tok = type === 'Bearer' ? token : undefined;
+
+      await this.jwtService.verifyAsync(token);
+      return res.status(200).json({ isValid: true });
+
+    } catch (error) {
+      return res.status(200).json({ isValid: false, message: error.message });
+    }
   }
 }
