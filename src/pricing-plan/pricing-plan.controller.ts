15f9a6869fda97f682ae24cf7e63c7ef7913700b
NicolasVelasco <nicolas.velasco@alumnos.frm.utn.edu.ar>
2024-11-20 16:02:49 -0300

    Final

--- src/pricing-plan/pricing-plan.controller.ts
+++ src/pricing-plan/pricing-plan.controller.ts
@@ -1,34 +1,100 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, HttpStatus, UseGuards } from '@nestjs/common';
 import { PricingPlanService } from './pricing-plan.service';
 import { CreatePricingPlanDto } from './dto/create-pricing-plan.dto';
 import { UpdatePricingPlanDto } from './dto/update-pricing-plan.dto';
+import { ApiTags, ApiBody, ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from '../auth/guards/auth.guard';
 
-@Controller('pricing-plan')
+@ApiTags('Pricing-Plan')
+@Controller('pricingplan')
 export class PricingPlanController {
   constructor(private readonly pricingPlanService: PricingPlanService) {}
 
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth('bearerAuth')
   @Post()
-  create(@Body() createPricingPlanDto: CreatePricingPlanDto) {
-    return this.pricingPlanService.create(createPricingPlanDto);
+  @ApiBody({
+    type: CreatePricingPlanDto,
+  examples: {
+    example: {
+      value: {
+        name: "PricingPlan name",
+        description: "Description PricingPlan",
+        price: 1500
+      }
+    }
+  }})
+  async create(@Body() createPricingPlanDto: CreatePricingPlanDto) {
+    try {
+      return await this.pricingPlanService.create(createPricingPlanDto);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
   }
 
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth('bearerAuth')
   @Get()
-  findAll() {
-    return this.pricingPlanService.findAll();
+  async getAll() {
+    try {
+      return await this.pricingPlanService.getAll();
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.NOT_FOUND);
+    }
   }
 
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth('bearerAuth')
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.pricingPlanService.findOne(+id);
+  async getOne(@Param('id') id: string) {
+    try {
+      return await this.pricingPlanService.getOne(id);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.NOT_FOUND);
+    }
   }
 
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth('bearerAuth')
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePricingPlanDto: UpdatePricingPlanDto) {
-    return this.pricingPlanService.update(+id, updatePricingPlanDto);
+  @ApiBody({
+    type: CreatePricingPlanDto,
+  examples: {
+    example: {
+      value: {
+        name: "PricingPlan name",
+        description: "Description PricingPlan",
+        price: 1500
+      }
+    }
+  }})
+  async update(@Param('id') id: string, @Body() updatePricingPlanDto: UpdatePricingPlanDto) {
+    try {
+      return await this.pricingPlanService.update(id, updatePricingPlanDto);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.NOT_FOUND);
+    }
   }
 
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth('bearerAuth')
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.pricingPlanService.remove(+id);
+  async softDelete(@Param('id') id: string) {
+    try {
+      await this.pricingPlanService.softDelete(id);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth('bearerAuth')
+  @Patch('/recover/:id')
+  async recover(@Param('id') id: string) {
+    try {
+      return await this.pricingPlanService.recover(id);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.NOT_FOUND);
+    }
   }
 }
--- src/pricing-plan/pricing-plan.service.ts
+++ src/pricing-plan/pricing-plan.service.ts
@@ -1,26 +1,99 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreatePricingPlanDto } from './dto/create-pricing-plan.dto';
 import { UpdatePricingPlanDto } from './dto/update-pricing-plan.dto';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PricingPlanService {
-  create(createPricingPlanDto: CreatePricingPlanDto) {
-    return 'This action adds a new pricingPlan';
+export class PricingPlanService extends PrismaClient implements OnModuleInit{
+
+  constructor() {
+    super();
   }
 
-  findAll() {
-    return `This action returns all pricingPlan`;
+  async onModuleInit() {
+    await this.$connect();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pricingPlan`;
+  async create(createPricingPlanDto: CreatePricingPlanDto) {
+    try {
+      return this.pricingPlan.create({
+        data: createPricingPlanDto,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
-  update(id: number, updatePricingPlanDto: UpdatePricingPlanDto) {
-    return `This action updates a #${id} pricingPlan`;
+  async getAll() {
+    try {
+      return this.pricingPlan.findMany({
+        where: { available: true, deletedAt: { gte: new Date() } },
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pricingPlan`;
+  async getOne(id: string) {
+    try {
+      const pricingPlan = await this.pricingPlan.findUnique({
+        where: { id },
+      });
+  
+      if (!pricingPlan || !pricingPlan.available || pricingPlan.deletedAt < new Date()) {
+        throw new NotFoundException('Pricing Plan not found');
+      }
+  
+      return pricingPlan;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async update(id: string, updatePricingPlanDto: UpdatePricingPlanDto) {
+    try {
+      const existing = await this.getOne(id);
+
+      return this.pricingPlan.update({
+        where: { id: existing.id },
+        data: updatePricingPlanDto,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async softDelete(id: string) {
+    try {
+      const existing = await this.getOne(id);
+
+      await this.pricingPlan.update({
+        where: { id: existing.id },
+        data: { available: false, deletedAt: new Date() },
+      });
+      return{ message: `Enterprise with ID ${id} has been soft deleted successfully.` };
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async recover(id: string) {
+    try {
+      const pricingPlan = await this.pricingPlan.findUnique({
+        where: { id },
+      });
+  
+      if (!pricingPlan || pricingPlan.available == true) {
+        throw new NotFoundException('Pricing Plan not found or already active');
+      }
+  
+      return this.pricingPlan.update({
+        where: { id },
+        data: { available: true, deletedAt: new Date('9999-12-12T00:00:00.000Z') },
+      });
+      
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
