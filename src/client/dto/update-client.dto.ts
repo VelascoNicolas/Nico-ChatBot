b97cf1ff7b9c204170dc4427be20e82ca26e7619
NicolasVelasco <nicolas.velasco@alumnos.frm.utn.edu.ar>
2024-11-20 16:17:15 -0300

    Fix Update DTO

--- src/client/dto/update-client.dto.ts
+++ src/client/dto/update-client.dto.ts
@@ -1,10 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateClientDto } from './create-client.dto';
-import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateClientDto extends PartialType(CreateClientDto) {
-    
-    @IsString()
-    @IsNotEmpty()
     id: string;
 }
--- src/pricing-plan/dto/update-pricing-plan.dto.ts
+++ src/pricing-plan/dto/update-pricing-plan.dto.ts
@@ -1,10 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePricingPlanDto } from './create-pricing-plan.dto';
-import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdatePricingPlanDto extends PartialType(CreatePricingPlanDto) {
-    
-    @IsString()
-    @IsNotEmpty()
     id: string;
 }
