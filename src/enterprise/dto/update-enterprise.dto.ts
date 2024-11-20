ae7385adce7c40127673dd235216fd2a5ff3f8c9
NicolasVelasco <nicolas.velasco@alumnos.frm.utn.edu.ar>
2024-11-20 16:10:57 -0300

    Update Enterprise

--- src/enterprise/dto/update-enterprise.dto.ts
+++ src/enterprise/dto/update-enterprise.dto.ts
@@ -1,9 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateEnterpriseDto } from './create-enterprise.dto';
-import { IsNotEmpty, IsString } from 'class-validator';
+
 
 export class UpdateEnterpriseDto extends PartialType(CreateEnterpriseDto) {
-    @IsString()
-    @IsNotEmpty()
     id: string;
 }
