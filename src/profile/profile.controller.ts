6307393c9ea0883bb96e05401e92ba6772cf6492
NicolasVelasco <nicolas.velasco@alumnos.frm.utn.edu.ar>
2024-11-21 19:37:17 -0300

    nicotina

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -129,7 +129,7 @@ export class ProfileController {
         throw new HttpException('File not found', 400);
       }
 
-      const filePath = './uploads/' + file.originalname;
+      const filePath = 'dist/uploads/' + file.originalname;
       await fs.promises.writeFile(filePath, file.buffer);
 
       return {
